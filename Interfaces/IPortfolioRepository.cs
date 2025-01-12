6ba933305217e0a24a8ac114334e82fbe277009e
tingzhong <tingzhong666@gmail.com>
2025-01-12 23:08:44 +0800

    -

---
+++ Interfaces/IPortfolioRepository.cs
@@ -0,0 +1,9 @@
+﻿using finance.api.Models;
+
+namespace finance.api.Interfaces
+{
+	public interface IPortfolioRepository
+	{
+		Task<List<Stock>> GetUserPortfolio(AppUser user);
+	}
+}
---
+++ Models/Portfolio.cs
@@ -0,0 +1,10 @@
+﻿namespace finance.api.Models
+{
+	public class Portfolio
+	{
+		public string AppUserId { get; set; }
+		public int StockId { get; set; }
+		public AppUser AppUser { get; set; }
+		public Stock Stock { get; set; }
+	}
+}
